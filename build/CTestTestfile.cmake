e06d59857b45ede2234c54332ff653884819d884
Cozma Vlad <vladuzz208@gmail.com>
2025-04-05 10:50:54 +0300

    Structure change

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/vladcozma/Hotel-Management-System
+# Build directory: /Users/vladcozma/Hotel-Management-System/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(HotelManagementTests "/Users/vladcozma/Hotel-Management-System/build/HotelManagementTests")
+set_tests_properties(HotelManagementTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/vladcozma/Hotel-Management-System/CMakeLists.txt;42;add_test;/Users/vladcozma/Hotel-Management-System/CMakeLists.txt;0;")
+subdirs("_deps/googletest-build")
